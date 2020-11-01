ee13b64a7e1bc211c6bfa6da11ce73c5a2c17232
WYAISD <18281045278@163.com>
2020-11-02 01:09:28 +0800

    更新

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"乖乖",'url':"/2020/10/26/RR.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"乖乖",'url':"/2020/10/26/RR.html"},{'title':"Markdown 语法笔记",'url':"/2020/11/02/Markdown-%E8%AF%AD%E6%B3%95.html"}]};
