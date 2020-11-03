4f3d16a4ae6821087bdc3380bee2c8fb9d40d644
WYAISD <18281045278@163.com>
2020-11-04 00:02:39 +0800

    更新

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"乖乖",'url':"/2020/10/26/RR.html"},{'title':"Markdown 语法笔记",'url':"/2020/11/02/Markdown-%E8%AF%AD%E6%B3%95.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"乖乖",'url':"/2020/10/26/RR.html"},{'title':"Markdown 语法",'url':"/2020/11/02/Markdown-%E8%AF%AD%E6%B3%95.html"},{'title':"定位",'url':"/2020/11/03/%E5%AE%9A%E4%BD%8D.html"}]};
